3e33e70aa03feed397f16e01c67c8d160d703c4c
smamery95 <98876561+smamery95@users.noreply.github.com>
2025-12-28 18:34:52 +0000

    creation_des_autres_request

---
+++ app/Http/Requests/ActivityRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ActivityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+       $rules= [
+            //
+            'city'=>['name','string','unique:activities,city','max:50'],
+
+        ];
+
+
+        if($this->method()==="PUT"){
+            $rules=array_merge($rules,[
+                'name'=>['sometimes','required','max:100','unique:activities,name'],
+
+            ]);
+        }
+        return $rules;
+    }
+}
---
+++ app/Http/Requests/LocationRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Client\HttpClientException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class LocationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+       $rules= [
+            //
+            'city'=>['required','string','unique:locations,city','max:50'],
+            'long'=>['required','numeric'],
+            'lat'=>['required','numeric'],
+        ];
+
+
+        if($this->method()==="PUT"){
+            $rules=array_merge($rules,[
+                'city'=>['sometimes','required','max:100'],
+                'long'=>['sometimes','required','numeric'],
+                'lat'=>['sometimes','required','numeric'],
+            ]);
+        }
+        return $rules;
+
+    }
+
+
+    public function failedValidation(Validator $validator)
+    {
+     throw new HttpResponseException(
+
+        response()->json([
+            'errors'=>$validator->errors(),
+
+        ],Response::HTTP_BAD_REQUEST)
+    );
+
+    }
+
+
+}
